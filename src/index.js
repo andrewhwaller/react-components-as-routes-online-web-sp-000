00c2e7edd50e26c681f5d81917d9ecedbc9213ac
Andrew Waller <andrewhwaller@gmail.com>
2019-09-28 15:52:26 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,72 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-import Home from './Home.js'
-
-const link = {
-  width: '100px',
-  padding: '12px',
-  margin: '0 6px 6px',
-  background: 'blue',
-  textDecoration: 'none',
-  color: 'white',
-}
-
-const Navbar = () =>
-  <div>
-    <NavLink
-      to="/"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-      >Home</NavLink>
-      <NavLink
-        to="/about"
-        exact
-        style={link}
-        activeStyle={{
-          background: 'darkblue'
-        }}
-        >About</NavLink>
-      <NavLink
-      to="/login"
-      exactstyle={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-      >Login</NavLink>
-  </div>;
-
-const About = () => <h1>This is my about component!</h1>;
-
-const Login = () => {
-  return (
-    <div>
-      <form>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-          <label htmlFor="username">Username</label>
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-          <label htmlFor="password">Password</label>
-        </div>
-        <input type="submit" value="Login" />
-      </form>
-    </div>
-  );
-};
-
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Navbar from './Navbar'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+ 
 ReactDOM.render((
   <Router>
-    <divt>
+    <div>
       <Navbar />
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/login" component={Login} />
-    </divt>
+    </div>
   </Router>),
   document.getElementById('root')
 );
